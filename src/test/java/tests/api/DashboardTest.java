c9dac630e5ebee8557e7a02f951d801d6f559ef2
David Kadasiev <dkadasiev@gmail.com>
2025-04-29 16:15:32 +0400

    API Test

--- src/test/java/tests/api/DashboardTest.java
+++ src/test/java/tests/api/DashboardTest.java
@@ -1,34 +1,52 @@
 package tests.api;
 
 import static io.restassured.RestAssured.given;
+import static org.testng.Assert.assertEquals;
 import static util.DashboardCreator.getDashboard;
 import static util.TestDataReader.getValue;
 
+import io.restassured.response.Response;
 import model.Dashboard;
 import org.json.JSONObject;
 import org.testng.annotations.Test;
 
 public class DashboardTest {
 
-  public static final String CREATE_DASHBOARD_ENDPOINT = "https://demo.reportportal.io/api/v1/default_personal/dashboard";
+  private static final String CREATE_DASHBOARD_ENDPOINT = "https://demo.reportportal.io/api/v1/default_personal/dashboard";
+  private static final String GET_DASHBOARD_BY_ID_ENDPOINT = "https://demo.reportportal.io/api/v1/default_personal/dashboard/";
 
-  @Test
-  public void createDashboard() {
+  private String dashboardId;
+
+  @Test(groups = {"run"})
+  public void verifyThatDashboardIsCreated() {
     Dashboard dashboard = getDashboard();
 
-    given()
+    Response response = given()
         .contentType("application/json")
         .header("Authorization", getValue("authorization"))
         .body(dashboard)
     .when()
-        .post(CREATE_DASHBOARD_ENDPOINT)
+        .post(CREATE_DASHBOARD_ENDPOINT);
+
+    dashboardId = response.jsonPath().getString("id");
+
+    assertEquals(response.statusCode(), 201, "Dashboard isn't created!");
+  }
+
+  @Test(dependsOnMethods = {"verifyThatDashboardIsCreated"})
+  public void verifyThatDashboardWasCreated() {
+    given()
+        .contentType("application/json")
+        .header("Authorization", getValue("authorization"))
+    .when()
+        .get(GET_DASHBOARD_BY_ID_ENDPOINT + dashboardId)
     .then()
-        .statusCode(201)
+        .statusCode(200)
         .log().all();
   }
 
   @Test
-  public void createDashboardWithWrongParameters() {
+  public void verifyThatDashboardIsNotCreated() {
     Dashboard dashboard = getDashboard();
 
     JSONObject data = new JSONObject();
